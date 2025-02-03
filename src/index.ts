df3e7dc78bb91a22460844212cf861387cf994b0
imsidkg <siddharth8271skg2@gmail.com>
2025-02-03 23:51:34 +0530

    added caching

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,106 @@
-import http from 'http';
-import axios from 'axios';
-import https from 'https';
+import http from "http";
+import axios from "axios";
+import https from "https";
 
 const backendServer = [
-    'https://jsonplaceholder.typicode.com',
-    'https://catfact.ninja',
-    'https://api.publicapis.io',  
-    'https://dog.ceo/api',
-    'https://api.thecatapi.com/v1',
-    'https://api.adviceslip.com',
-    'https://www.boredapi.com/api',
+  "https://jsonplaceholder.typicode.com",
+  "https://catfact.ninja",
+  "https://api.publicapis.io",
+  "https://dog.ceo/api",
+  "https://api.thecatapi.com/v1",
+  "https://api.adviceslip.com",
+  "https://www.boredapi.com/api",
 ];
 
-
-const testEndpoints  : {[key : string] : string }  = {
-    'https://jsonplaceholder.typicode.com': '/posts/1',
-    'https://catfact.ninja': '/fact',
-    'https://dog.ceo/api': '/breeds/image/random',
-    'https://api.thecatapi.com/v1': '/images/search',
-    'https://api.adviceslip.com': '/advice',
-    'https://www.boredapi.com/api': '/activity',
-    'https://api.publicapis.org': '/entries',
+const testEndpoints: { [key: string]: string } = {
+  "https://jsonplaceholder.typicode.com": "/posts/1",
+  "https://catfact.ninja": "/fact",
+  "https://dog.ceo/api": "/breeds/image/random",
+  "https://api.thecatapi.com/v1": "/images/search",
+  "https://api.adviceslip.com": "/advice",
+  "https://www.boredapi.com/api": "/activity",
+  "https://api.publicapis.org": "/entries",
 };
 
 let currentServerIndex: number = 0;
 
+let cacheObject: {
+  [key: string]: {
+    data: any;
+    headers: { [key: string]: string | string[] };
+    timestamp: number;
+  };
+} = {};
+
+const TTL = 5 * 60 * 1000;
+
 const httpServer = http.createServer(async (req, res) => {
-    try {
-        if (currentServerIndex >= backendServer.length) {
-            currentServerIndex = 0;
-        }
-
-        const currentServer = backendServer[currentServerIndex];
-        const endpoint = req.url === '/' ? testEndpoints[currentServer] : req.url;
-
-        console.log(`Method: ${req.method}`);
-        console.log(`Server: ${currentServer}`);
-        console.log(`Endpoint: ${endpoint}`);
-        console.log(`Server Index: ${currentServerIndex}`);
-
-        const forwardHeaders = {
-            'content-type': req.headers['content-type'],
-            'accept': req.headers['accept'],
-            'user-agent': 'proxy-server'
-        };
-
-        const response = await axios({
-            method: req.method,
-            url: `${currentServer}${endpoint}`,
-            headers: forwardHeaders,
-            httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-        });
-
-        const headers: {[key:string] : string | string[]} = {};
-        for(const key in response.headers) {
-            if(response.headers.hasOwnProperty(key) && response.headers[key] !== undefined) {
-                headers[key] = response.headers[key];
-            }
-        }
-
-        currentServerIndex++;
-        
-        res.writeHead(response.status, headers);
-        res.end(JSON.stringify(response.data));
-    } catch (error) {
-        console.error('Error forwarding request:', error);
-        if (axios.isAxiosError(error)) {
-            console.error('Response status:', error.response?.status);
-            console.error('Response data:', error.response?.data);
-        }
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
+  try {
+    if (currentServerIndex >= backendServer.length) {
+      currentServerIndex = 0;
     }
+
+    const currentServer = backendServer[currentServerIndex];
+    const endpoint = req.url === "/" ? testEndpoints[currentServer] : req.url;
+
+    const cacheKey = `${req.method}:${endpoint}`;
+
+    const cachedResponse = cacheObject[cacheKey];
+    const currentTime = Date.now();
+
+    if (cachedResponse && currentTime - cachedResponse.timestamp < TTL) {
+      res.writeHead(200, cachedResponse.headers);
+      res.end(JSON.stringify(cachedResponse.data));
+      return;
+    }
+
+    console.log(`Method: ${req.method}`);
+    console.log(`Server: ${currentServer}`);
+    console.log(`Endpoint: ${endpoint}`);
+    console.log(`Server Index: ${currentServerIndex}`);
+
+    const forwardHeaders = {
+      "content-type": req.headers["content-type"],
+      accept: req.headers["accept"],
+      "user-agent": "proxy-server",
+    };
+
+    const response = await axios({
+      method: req.method,
+      url: `${currentServer}${endpoint}`,
+      headers: forwardHeaders,
+      httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+    });
+
+    const headers: { [key: string]: string | string[] } = {};
+    for (const key in response.headers) {
+      if (
+        response.headers.hasOwnProperty(key) &&
+        response.headers[key] !== undefined
+      ) {
+        headers[key] = response.headers[key];
+      }
+    }
+
+    cacheObject[cacheKey] = {
+      data: response.data,
+      headers: headers,
+      timestamp: currentTime,
+    };
+
+    currentServerIndex++;
+
+    res.writeHead(response.status, headers);
+    res.end(JSON.stringify(response.data));
+  } catch (error) {
+    console.error("Error forwarding request:", error);
+    if (axios.isAxiosError(error)) {
+      console.error("Response status:", error.response?.status);
+      console.error("Response data:", error.response?.data);
+    }
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Internal Server Error");
+  }
 });
 
 httpServer.listen(3000, () => console.log("listening on port 3000"));
